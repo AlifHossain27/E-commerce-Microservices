5fbcbb29b0ec916957431eadfe1670fa9afb4eb8
AlifHossain27 <alifh044@gmail.com>
2024-08-30 07:52:24 +0600

    Updating Product Service

--- Product-Service/main.py
+++ Product-Service/main.py
@@ -2,10 +2,20 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .routers.product_router import product_router
 from .routers.category_router import category_router
-from .db import Base, engine, get_db
+from .routers.product_image_router import product_image_router
+from .db import Base, engine
 
 
-app = FastAPI()
+app = FastAPI(
+    title = "Product Service",
+    description = "An API to manage products",
+    version = "1.0.0",
+    contact = {
+        "name": "API Support",
+        "url": "https://github.com/AlifHossain27/E-commerce-Microservices",
+        "email": "alifh044@gmail.com"
+    }
+)
 
 Base.metadata.create_all(bind=engine)
 
@@ -21,4 +31,5 @@ app.add_middleware(
 app.include_router(category_router, tags=["Categories"], prefix="/api")
 # Product router
 app.include_router(product_router, tags=["Products"], prefix="/api")
-
+# Product Image router
+app.include_router(product_image_router, tags=["Product Image"], prefix="/api")
